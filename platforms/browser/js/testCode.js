15f70b16cde83683cb104f96a14b545c3215a1b6
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-03-08 23:45:55 +0000

    Tried a few things but couldn't get separate js function to call from html file...

--- platforms/browser/js/testCode.js
+++ platforms/browser/js/testCode.js
@@ -1,12 +1,4 @@
-function init() {
-    // the next line makes it impossible to see Contacts on the HTC Evo since it
-    // doesn't have a scroll button
-    // document.addEventListener("touchmove", preventBehavior, false);
-    document.addEventListener("deviceready", deviceInfo, true);
+function changeColor(){
+    document.getElementById("title").style.color = "red";
 }
 
-function msg()
-{
-	document.getElementById("title").style.color = "red";
-    alert("button clicked");
-}
