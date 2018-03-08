07e07da5a405e484c470bcd8dcaac50e1c3600da
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-03-08 19:19:52 +0000

    Button boi
    button test, tried to make it work with 'msg()' but did it in html file instead but will get it to work in the js file after

---
+++ platforms/browser/js/testCode.js
@@ -0,0 +1,12 @@
+function init() {
+    // the next line makes it impossible to see Contacts on the HTC Evo since it
+    // doesn't have a scroll button
+    // document.addEventListener("touchmove", preventBehavior, false);
+    document.addEventListener("deviceready", deviceInfo, true);
+}
+
+function msg()
+{
+	document.getElementById("title").style.color = "red";
+    alert("button clicked");
+}
