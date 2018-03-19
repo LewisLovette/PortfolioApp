6bf37e69c84b246bb1bb7f5fa1c951591b70fef5
Lewis Lovette <lewislovette3@hotmail.co.uk>
2018-03-19 21:53:29 +0000

    Added Plugins, Actually really fun - cont below~
    Time flies when doing this, soo much to learn - Gonna try start doing a bit each day to speed up the learning. - Next task to get the graph working!

--- platforms/browser/www/testCode.js
+++ platforms/browser/www/testCode.js
@@ -1,7 +1,4 @@
 function init() {
-    // the next line makes it impossible to see Contacts on the HTC Evo since it
-    // doesn't have a scroll button
-    // document.addEventListener("touchmove", preventBehavior, false);
     document.addEventListener("deviceready", deviceInfo, true);
 }
 
@@ -18,10 +15,36 @@ function onDeviceReady(){
   // navigator.notification.alert("PhoneGap is working");
 }
 
+//Button Page Navigation
+function back(){
+  window.history.back();
+}
+
 function mainToPortfolio(){
 	window.location = "portfolioPage.html";
 }
-
-function portfolioToMain(){
-	window.location = "index.html";
+function mainToTrends(){
+  window.location = "trendsPage.html";
 }
+
+//Setting Up Graph -- Test At the moment.
+var options = {
+   "IECanvasHTC": "/plotkit/iecanvas.htc",
+   "colorScheme": PlotKit.Base.palette(PlotKit.Base.baseColors()[0]),
+   "padding": {left: 0, right: 0, top: 10, bottom: 30},
+   "xTicks": [{v:0, label:"zero"}, 
+          {v:1, label:"one"}, 
+          {v:2, label:"two"},
+          {v:3, label:"three"},
+          {v:4, label:"four"}],
+   "drawYAxis": false
+};
+function drawGraph() {
+    var layout = new PlotKit.Layout("bar", options);
+    layout.addDataset("sqrt", [[0, 0], [1, 1], [2, 1.414], [3, 1.73], [4, 2]]);
+    layout.evaluate();
+    var canvas = MochiKit.DOM.getElement("graph");
+    var plotter = new PlotKit.SweetCanvasRenderer(canvas, layout, options);
+    plotter.render();
+}
+MochiKit.DOM.addLoadEvent(drawGraph);
